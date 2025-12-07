faa1b00a129ac77238576c516fe094f2b2748ad6
Aravindh-2002-17 <aravindh7675@gmail.com>
2025-12-07 22:00:12 +0530

    Fix knexfile paths for Render

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -1,16 +1,3 @@
-// require("dotenv").config();
-
-// module.exports = {
-//   client: "mysql2",
-//   connection: {
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT,
-//   },
-//   pool: { min: 0, max: 7 },
-// };
 require("dotenv").config();
 const path = require("path");
 
@@ -26,10 +13,10 @@ module.exports = {
   pool: { min: 0, max: 7 },
 
   migrations: {
-    directory: path.join(__dirname, "../migrations"),
+    directory: path.join(__dirname, "src/migrations"),
   },
 
   seeds: {
-    directory: path.join(__dirname, "../seeds"),
+    directory: path.join(__dirname, "src/seeds"),
   },
 };
