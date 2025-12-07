f641ca8ecd7ba321bf32a7f2b39b209db7ea2b83
Aravindh-2002-17 <aravindh7675@gmail.com>
2025-12-07 21:48:34 +0530

    Fix knexfile migration path

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -1,4 +1,18 @@
+// require("dotenv").config();
+
+// module.exports = {
+//   client: "mysql2",
+//   connection: {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+//   },
+//   pool: { min: 0, max: 7 },
+// };
 require("dotenv").config();
+const path = require("path");
 
 module.exports = {
   client: "mysql2",
@@ -10,4 +24,12 @@ module.exports = {
     port: process.env.DB_PORT,
   },
   pool: { min: 0, max: 7 },
+
+  migrations: {
+    directory: path.join(__dirname, "../migrations"),
+  },
+
+  seeds: {
+    directory: path.join(__dirname, "../seeds"),
+  },
 };
