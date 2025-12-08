53e5306a158d4563f130abddf8ba510c3a16bcac
Aravindh-2002-17 <aravindh7675@gmail.com>
2025-12-08 20:24:25 +0530

    Switch to PostgreSQL for Render

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -2,21 +2,22 @@ require("dotenv").config();
 const path = require("path");
 
 module.exports = {
-  client: "mysql2",
+  client: "pg", // <--- IMPORTANT
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }, // <--- REQUIRED FOR RENDER
   },
   pool: { min: 0, max: 7 },
 
   migrations: {
-    directory: path.join(__dirname, "..", "..", "migrations"),
+    directory: path.join(__dirname, "../migrations"),
   },
 
   seeds: {
-    directory: path.join(__dirname, "..", "..", "seeds"),
+    directory: path.join(__dirname, "../seeds"),
   },
 };
